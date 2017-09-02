a5929bdebb7405943861466fa514096eecb7b4d1
ohjonah <ohjonah@gmail.com>
2017-09-02 13:02:24 -0700

    Adds NoteCreateForm

---
+++ src/component/app/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import ReactDom from 'react-dom';
+import {BrowserRouter, Route} from 'react-router-dom';
+
+import NoteCreateForm from '../note-create-form';
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+
+    }
+  }
+}
+
+export default App;
---
+++ src/component/note-create-form/index.js
@@ -0,0 +1,53 @@
+import React from 'react';
+import uuid from 'uuid/v1';
+
+class NoteCreateForm extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      title: '',
+      content: '',
+      editing: false,
+      completed: false
+    };
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(e) {
+    this.setState({
+      [e.target.name]: e.target.value
+    });
+  }
+
+  handleSubmit(e) {
+    e.preventDefault();
+    this.props.handleNoteCreate(this.state);
+  }
+
+  render() {
+    return (
+      <form onSubmit={this.handleSubmit}>
+        <input
+          name='title'
+          type='text'
+          value={this.state.title}
+          onChange={this.handleChange}
+          />
+
+        <input
+          name='content'
+          type='text'
+          value={this.state.content}
+          onChange={this.handleChange}
+          />
+
+        <button type='submit'>Add Note</button>
+      </form>
+    );
+  }
+}
+
+export default NoteCreateForm;
